b6fdae0d6ad87bc168e095d4b6028571bde4e580
GabrielAssad <131619537+gabriel-assad@users.noreply.github.com>
2024-09-27 19:32:06 -0300

    Add files via upload

--- prova01/Exercicio 2/Ex02.kt
+++ prova01/Exercicio 2/Ex02.kt
@@ -1,1 +1,20 @@
+fun ehBissexto(ano: Int): Boolean {
+    // Retorna verdadeiro se o ano for bissexto
+    return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)
+}
 
+fun verificarData(dia: Int, mes: Int, ano: Int): String {
+    return when (mes) {
+        1, 3, 5, 7, 8, 10, 12 -> if (dia in 1..31) "Data válida" else "Data inválida"  // Meses com 31 dias
+        4, 6, 9, 11 -> if (dia in 1..30) "Data válida" else "Data inválida"  // Meses com 30 dias
+        2 -> if (ehBissexto(ano) && dia in 1..29) "Data válida"  // Fevereiro em ano bissexto
+        else if (!ehBissexto(ano) && dia in 1..28) "Data válida"  // Fevereiro em ano não bissexto
+        else "Data inválida"
+        else -> "Data inválida"  // Se o mês não for válido
+    }
+}
+
+fun main() {
+    println(verificarData(29, 2, 2020))  // Exemplo: ano bissexto
+    println(verificarData(29, 2, 2021))  // Exemplo: ano não bissexto
+}
